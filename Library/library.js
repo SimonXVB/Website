e9004c4ed0bbd07ace382df1e4dcdb63f542f033
Simon <167611089+SimonXVB@users.noreply.github.com>
2024-05-27 12:36:37 +0200

    Add files via upload

--- Library/library.js
+++ Library/library.js
@@ -1,22 +1,19 @@
 const title = document.getElementById("title");
 const desc = document.getElementById("desc");
 const date = document.getElementById("date");
-const editDiv = document.getElementById("editDiv");
-const titleEdit = document.getElementById("titleEdit");
-const descEdit = document.getElementById("descEdit");
-const dateEdit = document.getElementById("dateEdit");
-const confirmEdit = document.getElementById("confirmEdit");
 const addBtn = document.getElementById("add-btn");
 const list = document.getElementById("list");
-let index = -1;
+let index = 0;
 const arr = [];
 
-    function CreateEntry(title, desc, date, index) {
+class Entry {
+    constructor(title, desc, date, index) {
         this.titleName = title;
         this.descName = desc;
         this.dateName = date;
         this.indexName = index;
     };
+};
 
 function stuff(){
     if(title.value === "" || desc.value === "" || date.value === ""){
@@ -24,7 +21,7 @@ function stuff(){
     }
     else{
             function pushToArr(){           
-                const obj = new CreateEntry(title.value, desc.value, date.value.split("-").reverse().join("-"), index = index + 1);
+                const obj = new Entry(title.value, desc.value, date.value.split("-").reverse().join("-"), index++);
                 arr.push(obj);
                 console.log(arr)
                 };
